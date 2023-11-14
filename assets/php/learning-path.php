e6b2333be9270e0789f9397d5c34bb6374ac5d22
Jay <amandagurns@hotmail.ca>
2023-11-14 09:02:35 -0500

    Minor fixes

--- assets/php/learning-path.php
+++ assets/php/learning-path.php
@@ -30,30 +30,30 @@ ini_set('display_errors', 1);
         }
     }
 
-    // Object for individual resources.
-    class Resources {
-        private $resourceName;
-        private $resourceUrl;
+    // // Object for individual resources.
+    // class Resources {
+    //     private $resourceName;
+    //     private $resourceUrl;
 
-        public function __construct($name, $url)
-        {
-            $this->resourceName = $name;
-            $this->resourceUrl = $url;
-        }
+    //     public function __construct($name, $url)
+    //     {
+    //         $this->resourceName = $name;
+    //         $this->resourceUrl = $url;
+    //     }
 
-        // Getter
-        public function __get($property) {
-            if (property_exists($this, $property)) {
-                return $this->$property;
-            }
-        }
-        // Setter
-        public function __set($property, $value) {
-            if (property_exists($this, $property)) {
-                $this->$property = $value;
-            }
-        }
-    }
+    //     // Getter
+    //     public function __get($property) {
+    //         if (property_exists($this, $property)) {
+    //             return $this->$property;
+    //         }
+    //     }
+    //     // Setter
+    //     public function __set($property, $value) {
+    //         if (property_exists($this, $property)) {
+    //             $this->$property = $value;
+    //         }
+    //     }
+    // }
 
     // Testing:
 
@@ -77,27 +77,3 @@ ini_set('display_errors', 1);
         // echo "<br>";
         // echo "<pre>"; print_r($objTest); echo "<pre>";
 ?>
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <!-- Learning paths with the ability to view, create, edit, and delete. -->
-    <!-- TO DO:
-            * storage for learning paths -->
-    <h3>Learning Paths: </h3>
-    <?php 
-        echo "Path Name: " . $objTest->__get('pathName') . "<br>";
-        echo "Path Creator: " . $objTest->__get('pathCreator') . "<br>";
-        echo "Path Description: " . $objTest->__get('pathDescription') . "<br>";
-        echo "Path Resource 1 Name: " . $objTest->__get('pathResources')['Resource 1']->__get('resourceName') . "<br>";
-        echo "Path Resource 1 URL: " . $objTest->__get('pathResources')['Resource 1']->__get('resourceUrl') . "<br>";
-        echo "Path Resource 2 Name: " . $objTest->__get('pathResources')['Resource 2']->__get('resourceName') . "<br>";
-        echo "Path Resource 2 URL: " . $objTest->__get('pathResources')['Resource 2']->__get('resourceUrl') . "<br>";
-        echo "<br>";
-        echo "<pre>"; print_r($objTest); echo "<pre>";
-    ?> 
--- assets/php/path-form.php
+++ assets/php/path-form.php
@@ -1,13 +1,16 @@
 <?php
+echo htmlspecialchars($_SERVER['PHP_SELF']);
 include "learning-path.php";
 
-
+// If user submits form...
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    if (isset($_POST['path-name']) && isset($_POST['path-desc']) && isset($_POST['given-resources1'])) {
+    // If name, desc, and resources are set...
+    if (isset($_POST['path-name']) && isset($_COOKIE['firstName']) && isset($_POST['path-desc']) && isset($_POST['given-resources1'])) {
         $pathName = $_POST['path-name'];
+        $pathUser = $_COOKIE['firstName'];
         $pathDescription = $_POST['path-desc'];
         $pathResources = array();
-
+        // Pull from counter to push array.
         if (isset($_POST['counter'])) {
             $counter = $_POST['counter'];
             for ($i = 1; $i < $counter; $i++) {
@@ -15,15 +18,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             }
         }
     }
-
-
 }
-
-
-
-    $test = new LearningPath($pathName, "Jay", $pathDescription, $pathResources);
+    $test = new LearningPath($pathName, $pathUser, $pathDescription, $pathResources);
     echo "<pre>"; print_r($test); echo "<pre>";
-
 ?>
-
-<?php echo htmlspecialchars($_SERVER['PHP_SELF']);?>
