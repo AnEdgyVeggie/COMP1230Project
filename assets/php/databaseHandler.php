a1331075263cb0d34cb46bf6c7ce5364a7d78a15
Ethan Sylvester <ethan.sylvester@gmx.com>
2023-11-18 15:01:55 -0500

    working on loading screen, issue with passing cookies to loading screen only

---
+++ assets/php/databaseHandler.php
@@ -0,0 +1,12 @@
+<?php
+    $dbServerName = "localhost";
+    $dbUsername = "root";
+    $dbPassword = "";
+    $dbName = "users";
+
+    $connection = mysqli_connect($dbServerName, $dbUsername, $dbPassword);
+
+
+
+
+?>
--- pages/loading/loadingScreen.php
+++ pages/loading/loadingScreen.php
@@ -1,37 +1,44 @@
 <?php
-include_once("assets/php/databaseHandler.php");
+
 ?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="assets/css/style.css">
-    <title>Welcome to Learning Paths</title>
+    <link rel="stylesheet" href="../../assets/css/loading.css">
+    <title>Document</title>
 </head>
 <body>
-    <header>
+<header>
         <nav>
             <span>
                 <a href="pages/learning-path.html">Learning Path HTML</a>|
                 <a href="assets/php/learning-path.php">Learning Path PHP</a>|
-                <a href="">Home</a>
+                <a href="../../index.html">Home</a>
             </span>
 
             <span id="login">
-                <a href="pages/register.html">Register</a>
+                <a href="../register.html">Register</a>
                 |
-                <a href="pages/login.html">Login</a>
+                <a href="../login.html">Login</a>
             </span>
         </nav>        
     </header>
-    <p>
-    <?php
-    if (isset($_COOKIE["firstName"])) 
-    {
-        
-        echo $_COOKIE["firstName"];
-    }
-    ?></p>
+
+    <!-- FOR LOADING SCREEN -->
+    <h1>Loading</h1>
+    <div id="d5">
+    </div>
+    <div id="d4">
+    </div>
+    <div id="d3">
+    </div>
+    <div id="d2">
+    </div>
+    <div id="d1">
+    </div>
+
 </body>
 </html>
